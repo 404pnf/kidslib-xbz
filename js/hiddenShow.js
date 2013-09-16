3ad666012f3b0d8f1eab7769738ec893df48ba10
404pnf <404pnf@exmaple.org>
2013-09-16 13:58:18 +0800

    init checkin

---
+++ js/hiddenShow.js
@@ -0,0 +1,16 @@
+
+
+//鼠标移入显示
+
+function userShow(){ 
+var ei = document.getElementById("user_name");
+ei.style.display = "block";
+}
+
+//鼠标移出隐藏
+
+function userHidden(){
+var ei = document.getElementById("user_name");
+ei.style.display = "none";
+}
+
