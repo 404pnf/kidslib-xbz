fe4c9f419381a0105a0ce4d54dd7e3309f019164
404pnf <404pnf@exmaple.org>
2013-10-10 11:11:29 +0800

    even better documentation

--- quiz.rb
+++ quiz.rb
@@ -8,58 +8,81 @@ ERUBY_TPL = 'views/quiz-eruby.html'
 OUTPUT = 'ouput'
 VIEWS = 'views'
 
-# csv format
-# "书名","nid","图片","音频","答案选项","unit_id"
+# ## CSV的格式
+# 每条记录有一个图片，一个音频，但答案选项有4个，用英文逗号分开。
+#
+#     "书名","nid","图片","音频","答案选项","unit_id"
+#     "2BU2_six","315","2BU2_six.jpg"," 2BU2_six.mp3","six, three, seven, five","2B_U2"
+#
+# ----
+
+
+
+# ## 主函数
+# FIXME TODO
+#
+# 相当基于步骤。相当不好。且选项的乱序是在脚本中混合了变量和html。真是相当丑陋啊。
+#
+# 更好的方法是把选项作为
+#
+#     [{answer: 'uncle', correct?: true} {answer: 'aunt', correct?: false}]
+#
+# 然后在模版中用
+#
+#     <li class="<% a[:correct?] ? 'correct-anser' : 'wrong-anser' "><%= a[:anser]></li>
+# ### 难点
+# 1. 先把对应相同单元的quiz按文件名命名规则收集在一起
+# 1. 每课可以对应多个quiz。因此quiz页面需要分页，并计算是否有上一页和下一页。
+# 1. 选项需要乱序
+# 1. 正确选项和错误选项对应的css class不同
+# ----
 def generate_quiz_html(path)
-	hash = Hash.new { |h, k| h[k] = [] }
-	c = CSV.read(path)
-	c.each_with_object(hash) { |e, o| hash[e[5]] << e }
-	hash.each do |unit_idx, q |
-		len = q.size
-		unit_id = unit_idx.downcase
-		q.each_with_index do |(shuming, nid, pic, mp3, choices, unit), idx|
-    	page_title = "#{unit_id}"
-    	pic.gsub!(/'/, '_')
-    	pic.gsub!(/ /, '_')
-			mp3.strip!
-			mp3.gsub!(/'/, '_') # 之前有文件名字中有单引号
-			mp3.gsub!(/ /, '_')
-			answers = choices.split(',')
-			choice_str = <<eof
-				<li><a href='#' onclick='onclick_right();'>#{answers[0]}</a></li>
-				<li><a href='#' onclick='onclick_wrong();'>#{answers[1]}</a></li>
-				<li><a href='#' onclick='onclick_wrong();'>#{answers[2]}</a></li>
-				<li><a href='#' onclick='onclick_wrong();'>#{answers[3]}</a></li>
+  hash = Hash.new { |h, k| h[k] = [] }
+  c = CSV.read(path)
+  c.each_with_object(hash) { |e, o| hash[e[5]] << e }
+  hash.each do |unit_idx, q |
+    len = q.size
+    unit_id = unit_idx.downcase
+    q.each_with_index do |(shuming, nid, pic, mp3, choices, unit), idx|
+      page_title = "#{unit_id}"
+      pic.gsub!(/'/, '_')
+      pic.gsub!(/ /, '_')
+      mp3.strip!
+      mp3.gsub!(/'/, '_') # 之前有文件名字中有单引号
+      mp3.gsub!(/ /, '_')
+      answers = choices.split(',')
+      choice_str = <<eof
+        <li><a href='#' onclick='onclick_right();'>#{answers[0]}</a></li>
+        <li><a href='#' onclick='onclick_wrong();'>#{answers[1]}</a></li>
+        <li><a href='#' onclick='onclick_wrong();'>#{answers[2]}</a></li>
+        <li><a href='#' onclick='onclick_wrong();'>#{answers[3]}</a></li>
 eof
-			randomized_choices = choice_str.split(/\n/).shuffle.join("\n")
-			prev_q = if idx == 0
-					nil
-				else
-			 	"<a href='#{unit_id}_quiz_#{idx - 1}.html' class='pre font_mrosoftYHB fleft'>上一题</a>"
-				end
-			next_q = if idx + 1 == len
-					nil
-				else
-				  "<a href='#{unit_id}_quiz_#{idx + 1}.html' class='next font_mrosoftYHB fleft'>下一题</a>"
-				end
-			input = File.read("#{VIEWS}/quiz-eruby.html")
-		  eruby = Erubis::Eruby.new(input)    # create Eruby object
-		  quiz_html =  eruby.result(binding) # get result
-		  p "生成 #{ unit_id }_quiz_#{ idx }.html "
-		  File.write("output/html/#{ unit_id }_quiz_#{ idx }.html", quiz_html)
-		end
-	end
+      randomized_choices = choice_str.split(/\n/).shuffle.join("\n")
+      prev_q = if idx == 0
+          nil
+        else
+         "<a href='#{unit_id}_quiz_#{idx - 1}.html' class='pre font_mrosoftYHB fleft'>上一题</a>"
+        end
+      next_q = if idx + 1 == len
+          nil
+        else
+          "<a href='#{unit_id}_quiz_#{idx + 1}.html' class='next font_mrosoftYHB fleft'>下一题</a>"
+        end
+      input = File.read("#{VIEWS}/quiz-eruby.html")
+      eruby = Erubis::Eruby.new(input)
+      quiz_html =  eruby.result(binding)
+      p "生成 #{ unit_id }_quiz_#{ idx }.html "
+      File.write("#{OUTPUT}/html/#{ unit_id }_quiz_#{ idx }.html", quiz_html)
+    end
+  end
 end
 
+# 复制静态资源到output
 def copy_asset_to_output
-	# If you want to copy all contents of a directory instead of the
-	# directory itself, c.f. src/x -> dest/x, src/y -> dest/y,
-	# use following code.
-	# cp_r('src', 'dest') makes dest/src,
-	# but this doesn't.
-	FileUtils.cp_r 'views/.', 'output', :verbose => true
+  FileUtils.cp_r 'views/.', "#{OUTPUT}", :verbose => true
 end
 
+# ## 干活
 if __FILE__ == $PROGRAM_NAME
   path = ARGV[0] || CSVFILE
   p "输入文件是#{ path }"
--- xbz-ebok.rb
+++ xbz-ebok.rb
@@ -3,36 +3,60 @@ require 'csv'
 require 'fileutils'
 require 'erubis'
 
-def gen_xbz_book_html(path)
-	# get mp3 and jpg tuples
-	# [["3b_u9_2.jpg", "3b_u9_2.mp3"], ["3b_u9_3.jpg", "3b_u9_3.mp3"]]
-	Dir.chdir path
-	f = Dir['*.jpg'].sort.zip Dir['*.mp3'].sort
-	p "共有图书 #{ f.size } 本"
-	Dir.chdir __dir__ # 回到脚本执行的目录
-	#p f
-	h = Hash.new { |hash, key| hash[key] = [] }
-	f.each_with_object(h) do |(jpg, mp3), o|
-		bookid = jpg.slice(0..4).to_sym
-		o[bookid] << [jpg, mp3]
-	end
-	#p h
-	h.each do |unit, pages|
-		len = pages.size
-		pages.each_with_index do |(jpg, mp3), idx|
-			basename = "#{ unit.downcase }_book"
-			prev_link = idx == 0 ? nil : "#{basename}_#{ idx - 1}.html"
-			next_link = idx == 2 ? nil : "#{basename}_#{ idx + 1}.html"
 
-			input = File.read('views/xbzebook-eruby.html')
-	    eruby = Erubis::Eruby.new(input)    # create Eruby object
-	    book_html =  eruby.result(binding) # get result
-	    File.write("output/html/#{basename}_#{idx}.html", book_html)
-	  end
+# ## 说明
+# 新标准的图书每页是一个jpg，并且有对应的mp3。它们统一放在某文件夹中。
+#
+# 我们去遍历那个文件夹，根据文件名称确定书名，并把同一本书的jpg和mp3收集在一起
+#
+# 之后我们生成可翻页的图书，每页都会自动播放对应的mp3。实现方式看erubis模版。
+#
+# ----
+
+# ## 主函数
+# TODO FIXME
+#
+# 写得很不好。拘泥于过程。需要修改
+3
+# ----
+def gen_xbz_book_html(path)
+  # get mp3 and jpg tuples
+  #
+  # [["3b_u9_2.jpg", "3b_u9_2.mp3"], ["3b_u9_3.jpg", "3b_u9_3.mp3"]]
+  Dir.chdir path
+  f = Dir['*.jpg'].sort.zip Dir['*.mp3'].sort
+  p "共有图书 #{ f.size } 本"
+  Dir.chdir __dir__ # 回到脚本执行的目录
+
+  # 书名是文件名的前5个字符(0..4)。
+  # 用它作为键，构建所有图书的hash
+  h = Hash.new { |hash, key| hash[key] = [] }
+  f.each_with_object(h) do |(jpg, mp3), o|
+    bookid = jpg.slice(0..4).to_sym
+    o[bookid] << [jpg, mp3]
+  end
+
+  # 先看看这本书一共有几页, page.size 。
+  #
+  # 再根据当前页面来决定是否有上一页和下一页。
+  h.each do |unit, pages|
+    len = pages.size
+    pages.each_with_index do |(jpg, mp3), idx|
+      basename = "#{ unit.downcase }_book"
+      prev_link = idx == 0 ? nil : "#{basename}_#{ idx - 1}.html"
+      next_link = idx == 2 ? nil : "#{basename}_#{ idx + 1}.html"
+
+      input = File.read('views/xbzebook-eruby.html')
+      eruby = Erubis::Eruby.new(input)    # create Eruby object
+      book_html =  eruby.result(binding) # get result
+      File.write("output/html/#{basename}_#{idx}.html", book_html)
+    end
   end
 end
+
+# ## 干活
 if __FILE__ == $PROGRAM_NAME
-	path = ARGV[0] || 'output/book'
-	p "输入文件是#{ path }"
-	gen_xbz_book_html(path)
+  path = ARGV[0] || 'output/book'
+  p "输入文件是#{ path }"
+  gen_xbz_book_html(path)
 end
